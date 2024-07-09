e6494c1531d2e75d2a2cd31c48a93b3a84aec98c
Amr Nasser <amrnasser92@gmail.com>
2024-07-09 19:45:31 +0300

    Starting Binding Calculation

---
+++ binding.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import numpy as np
+import gspread
+import re
+
+
+
+# TODO: Wiro Calculation
+# Calculate Paper Thickness
+
+# Paper Thickness = gsm * pages Number /2000 + 4 => Paper thickness in mm
+
+# Check which part of the format is the length
+
+# The wiro costs in the old pricing is supposed to be with setup cost of 200
+
+# Old Pricing has setup cost of 200
+
+# Setup Cost with Future Fusion is 350 and not 200
+
+
+# TODO: Pur Binding Calculation
+
+# Calculate the thickness
+
+# Old Pricing -> Setup Cost 350
+
