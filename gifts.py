0df8040d3a35c92fcbd90f1c8dc0eae851463266
Amr Nasser <amrnasser92@gmail.com>
2024-11-17 22:28:47 +0200

    Updated Gifts Calculation

--- gifts.py
+++ gifts.py
@@ -1,44 +1,43 @@
 import pandas as pd
 import numpy as np
-from helper_pricing import get_weights, get_shipping_costs, get_finishing_costs, get_litho_sf_SQM
-
+from helper_pricing import get_weights, get_shipping_costs, get_finishing_costs, get_litho_sf_SQM, calculate_attributes
+from shipping import calculate_shipping
 
 def calculation(df: pd.DataFrame)-> pd.DataFrame:
     df = df.reset_index(drop=True)
     print("Gifts Calculation Started  :", len(df))
 
-    df["GSM"] =df["Paper"].str.extract("(\d+)gsm")
+    df["GSM"] = df["Paper"].str.extract("(\d+)gsm")
     df["GSM"] = pd.to_numeric(df["GSM"], errors="coerce")
-    df["SQM"] = df["Format"].apply(get_litho_sf_SQM).astype('float32') # get_litho_sf_SQM(df["Format"])  # FIXME: Not Sure about SQM to get GSM, SQM should be based on sheet size, which sheet size to take if no printing
-    # df["SQM"] = df["Sheet Size"].apply(get_litho_sf_SQM).astype('float32')
+    df["SQM"] = df["Format"].apply(get_litho_sf_SQM).astype('float32')
+
     finishing = get_finishing_costs()
     finishing = finishing[(finishing["value"].isna() == False) & (finishing["Supplier"] != "Quantity")]
     finishing = finishing.reset_index(drop=True)
-    extra_costs = pd.merge(df[["Quantity", "Extra"]], finishing, "left", left_on="Extra", right_on="Attribute")
-    extra_costs["Extra_costs"] = extra_costs["Setup-Cost"] + extra_costs["Quantity"] * extra_costs["value"]
-    extra_costs["Extra_costs"] = np.where(extra_costs["Extra"] == "None", 0, extra_costs["value"] * extra_costs["Quantity"])
-    df["Extra_costs"] = extra_costs["Extra_costs"]
-    df["Supplier"] = extra_costs["Supplier"]
 
+    extra_costs = pd.merge(df[["Quantity", "Extra", "idx"]], finishing, "left", left_on="Extra", right_on="Attribute")
+    extra_costs["Extra_costs"] = extra_costs["Setup-Cost"].fillna(0) + extra_costs["Quantity"] * extra_costs["value"]
+    extra_costs["Extra_costs"] = np.where(extra_costs["Extra"] == "None", 0, extra_costs["Extra_costs"])
+
+    df = pd.merge(df, finishing, "left", left_on="Extra", right_on="Attribute")
+    df["Extra_costs"] = df["Setup-Cost"].fillna(0) + df["Quantity"] * df["value"]
+    df["Extra_costs"] = np.where(df["Extra"] == "None", 0, df["Extra_costs"])
 
     weights = get_weights()
-    extra_weights = weights[weights["Type"] == "Extra"]  # .reset_index(drop=True)
+    extra_weights = weights[weights["Type"] == "Extra"].reset_index(drop=True)
     extra_weights = df[["Extra"]].merge(extra_weights, "left", left_on="Extra", right_on="Attribute")
     df["Extra GSM"] = extra_weights["GSM"]
     del extra_weights
+    paper_weights = weights[weights["Type"] == "Paper"].reset_index(drop=True)
+    paper_weights = df[["Paper"]].merge(paper_weights, "left", left_on="Paper", right_on="Attribute")
+    df["GSM"] = paper_weights["GSM"]
+    df["GSM"].fillna(0)
     df["Extra GSM"] = df["Extra GSM"].fillna(0)
     df["Total Weight"] = df["GSM"] * df["SQM"] + df["Extra GSM"]
 
 # Shipping Costs
-
-    shipping_costs = get_shipping_costs()
-    df["Remaining"] = np.floor(df["Total Weight"] - shipping_costs["Minimum KG"])
-    df["Remaining"] = np.where(df["Remaining"] < 0, 0, df["Remaining"])
-    df["Shipping Costs"] = df["Remaining"] * shipping_costs["Kg After"] + shipping_costs["Minimum"]
-
-
-    df["Total Costs"] = df["Extra_costs"] + df["Shipping Costs"]  # FIX: Update Correct Values Later
-
+    df = calculate_shipping(df)
+    df["Total Costs"] = df["Extra_costs"] + df["Shipping Costs"]
     print("Gifts Calculation Ended  :", len(df))
     df = df.reset_index(drop=True)
     return df
