1072ee645b80642fbcd80dda25afbe32088aecba
Amr <amrnasser92@gmail.com>
2025-03-19 20:36:15 +0200

    Updated Fuel Costs

--- shipping.py
+++ shipping.py
@@ -15,18 +15,22 @@ def calculate_shipping(df: pd.DataFrame) -> pd.DataFrame:
     df["Remaining1"] = np.floor(df["Remaining1"])
     df["Remaining1"] = np.where(df["Remaining1"] < 0, 0, df["Remaining1"])
     df["Shipping Costs1"] = df["Remaining1"] * shipping_costs1["kg_after"] + shipping_costs1["minimum_cost"]
+    df["Shipping Costs1"] = df["Shipping Costs1"] * 1.2
 
 
     df["Remaining2"] = df["Total Weight"] - float(shipping_costs2["minimum_kg"])
     df["Remaining2"] = np.floor(df["Remaining2"])
     df["Remaining2"] = np.where(df["Remaining2"] < 0, 0, df["Remaining2"])
     df["Shipping Costs2"] = df["Remaining2"] * shipping_costs2["kg_after"] + shipping_costs2["minimum_cost"]
+    df["Shipping Costs2"] = df["Shipping Costs2"] * 1.2
 
 
     df["Remaining3"] = df["Total Weight"] - float(shipping_costs3["minimum_kg"])
     df["Remaining3"] = np.floor(df["Remaining3"])
     df["Remaining3"] = np.where(df["Remaining3"] < 0, 0, df["Remaining3"])
     df["Shipping Costs3"] = df["Remaining3"] * shipping_costs3["kg_after"] + shipping_costs3["minimum_cost"]
+    df["Shipping Costs3"] = df["Shipping Costs3"] * 1.2
+
 
     df["Shipping Costs"] = np.min(df[["Shipping Costs1", "Shipping Costs2", "Shipping Costs3"]], axis=1)
 
