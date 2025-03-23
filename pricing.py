b26c64d4592f10bdddb14b8c2aaf86aa411593fc
Amr <amrnasser92@gmail.com>
2025-03-23 06:33:26 +0200

    Updated Log for missing prices

--- pricing.py
+++ pricing.py
@@ -177,6 +177,7 @@ def pricing_calculation(file:str| Any) -> dict[Any, Any]:
     # output_data.to_csv(f"Output Data Before {products[0] if len(products) == 1 else None} {timestamp}.csv", index=False)
     # output_data[output_data["Total Costs"].isna()].to_csv(f"Output Data {products[0] if len(products) == 1 else None} {timestamp} no_prices.csv", index=False)
     output_data = output_data[output_data["Total Costs"].isna() == False]
+    failed_data = output_data[output_data["Total Costs"].isna()]
     print(len(output_data))
     # NOTE: First remove_duplicates from the same combination and keep one for each supplier with the lowest cost
     # Need to check for the same category
@@ -205,7 +206,7 @@ def pricing_calculation(file:str| Any) -> dict[Any, Any]:
     print(len(output_data))
     if len(output_data) == 0:
         logger.error("No Data")
-        raise
+        return ("failed",failed_data.to_dict(orient='records'))
     output_data = output_data.reset_index(drop=True)
     # output_data.to_csv(f"Output Data {products[0] if len(products) == 1 else None} {timestamp}.csv", index=False)
     output_data = output_data.sort_values("Total Costs", ascending=False)
@@ -220,7 +221,7 @@ def pricing_calculation(file:str| Any) -> dict[Any, Any]:
     # final_data.to_csv(f"./output/Final Data  {products[0] if len(products) == 1 else None} - {timestamp}.csv")
     # print(f"Final Data returned from Main: {type(final_data)}")
     data_to_send = final_data.reset_index()
-    return data_to_send.to_dict(orient='records')
+    return (success, data_to_send.to_dict(orient='records'))
 
 
 if __name__ == "__main__":
--- tasks.py
+++ tasks.py
@@ -45,8 +45,8 @@ def calculate_pricing_job(code):
     buffer.seek(0)
     logger.info("Created Combinations and running Prices")
     try:
-        final_prices = pricing_calculation(buffer)
-        if final_prices:
+        (code, final_prices) = pricing_calculation(buffer)
+        if code == "success":
             logger.info("Pricing Finished, Saving to Database")
             logger.info(final_prices)
             final_prices = [{str(k): price[k] for k in price.keys()} for price in final_prices]
@@ -55,6 +55,9 @@ def calculate_pricing_job(code):
             logger.info("Saved to MongoDB")
         else:
             logger.info(f"{code} generated no prices")
+            collection = db["pricing_logs"]
+            collection.insert_one({"timestamp": datetime.now(timezone.utc), "output":final_prices, "code":code})
+
     except Exception as e:
         logger.error(f"{code} failed: {str(e)}")
         collection = db["pricing_logs"]
