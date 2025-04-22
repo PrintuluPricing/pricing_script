9e811bb8691bb6078affd4dc746f5e1174633ff3
Amr <amrnasser92@gmail.com>
2025-04-22 03:05:35 +0200

    Updated directory to be manual

--- app.py
+++ app.py
@@ -67,12 +67,10 @@ def get_files():
 
 @app.route('/api/download/<product_code>', methods=['GET'])
 def download_prices(product_code):
-    # final_prices_df.to_csv(f"./prices/{code}_prices-{version}.csv", sep=";")
     product_prices = glob.glob(f"./prices/{product_code}_prices-*.csv")
     product_prices.sort(key= lambda x: x.split("-")[1].split(".")[0], reverse=True)
     latest_file = product_prices[0]
-    directory = os.path.join(app.root_path, "prices")
-    return send_from_directory(directory, latest_file, as_attachment=True)
+    return send_from_directory("./prices", latest_file, as_attachment=True)
 
 
 @app.route('/api/download-all/', methods=['GET'])
