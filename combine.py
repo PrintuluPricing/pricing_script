7c8b3949f0a5ca4978d13571bb883a1131f0f0bc
Amr <amrnasser92@gmail.com>
2025-07-13 05:19:45 +0300

    Fixed True typo

--- combine.py
+++ combine.py
@@ -36,7 +36,7 @@ attributes_cols = ['Pages', 'Finishing', 'Binding', 'Extra',
 def get_product_data(product_code: str) -> pd.DataFrame:
     db = client["Printulu"]
     products = db["products"]
-    product_data = products.find_one({"product_code": product_code, "active":true})
+    product_data = products.find_one({"product_code": product_code, "active":True})
     global_rules = db["settings"].find_one({"type":"Incompatibility Rules"})["incompatibility_rules"]
     rules = product_data.get("incompatibility_rules",[])
     rules.extend(global_rules)
