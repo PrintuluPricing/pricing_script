49abf6727adae16032f17c6262b512a505d37f6c
Amr <amrnasser92@gmail.com>
2025-07-22 20:32:54 +0300

    Added flag for testing SQL

--- pricing.py
+++ pricing.py
@@ -221,8 +221,7 @@ def pricing_calculation(file:str| Any, test=False, product_code=None) -> dict[An
     # Product Code, Quantity, Paper, Refinement, Finishing, Colour, Extra, Supplier, Binding, Printing Price, Refinement Price, Binding Price, Delivery Charges, Extra Price, 
     logger.info("Slicing the dataframe")
     sql_columns = ["productpart","Category","Pages","Finishing","Binding","Extra","Format","Quantity","Refinement","Colour","Paper","supplier","Shipping Costs","Refinement Costs","Extra Costs","Binding Costs","Finishing Costs","Total Printing Costs","Total Costs", "Placements", "printing_sheets", "Total Sheets", "GSM", "Paper Costs", "Printing and Paper Costs", "Ganging Possible", "machine"]
-    # if not test:
-    if True:
+    if not test:
         existing_columns = [col for col in sql_columns if col in output_data.columns]
         sql_data = output_data[existing_columns]
         sql_data[["Placements", "printing_sheets", "Total Sheets"]] = sql_data[["Placements", "printing_sheets", "Total Sheets"]].astype(int)
